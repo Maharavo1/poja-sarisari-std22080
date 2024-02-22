13c33a7c69b4b9323a002e8f97554d06f48f450f
Maharavo1 <hei.maharavo@gmail.com>
2024-02-22 12:20:54 +0300

    feat : Convert image to bkack and white

---
+++ src/main/java/school/hei/sarisary/endpoint/rest/controller/health/ConvertImageController.java
@@ -0,0 +1,25 @@
+package school.hei.sarisary.endpoint.rest.controller.health;
+
+
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import school.hei.sarisary.service.event.ConvertImageService;
+
+
+@RestController
+public class ConvertImageController {
+    private final ConvertImageService convertImageService;
+
+    public ConvertImageController(ConvertImageService convertImageService) {
+        this.convertImageService = convertImageService;
+    }
+
+    @PutMapping(value = "/blacks/{key}", produces = MediaType.IMAGE_PNG_VALUE)
+    public byte[] toBlackAndWhite(@PathVariable String key, @RequestBody byte[] image) {
+        return convertImageService.convertToBlackAndWhite(image, key);
+    }
+}
